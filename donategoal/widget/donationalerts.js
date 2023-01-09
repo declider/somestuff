a2c95c7b2faaf4b884399b7a0bb46d86158953c1
declider <62831308+declider@users.noreply.github.com>
2023-01-09 22:25:20 +0300

    Update donationalerts.js

--- donategoal/widget/donationalerts.js
+++ donategoal/widget/donationalerts.js
@@ -1,5 +1,5 @@
 async function getDAData() {
-    let res = await fetch('https://donategoalforhuman.deta.dev/dasocket?token='+dptoken, {
+    let res = await fetch('https://donategoalforhuman.deta.dev/dasocket?token='+datoken, {
         method: 'get',
         headers: {
             'Content-Type': 'application/json'
