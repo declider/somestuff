15e0ecbba2f1e720900f02f0e8e3ddfaf29d4b92
declider <62831308+declider@users.noreply.github.com>
2023-01-09 18:03:07 +0300

    Update donatepay.js

--- donategoal/widget/donatepay.js
+++ donategoal/widget/donatepay.js
@@ -2,8 +2,6 @@ async function getUserId() {
     let res = await fetch('https://donatepay.ru/api/v1/user?access_token='+dptoken, {
         method: 'get',
         headers: {
-            "Access-Control-Allow-Headers": "*", // this will allow all CORS requests
-            "Access-Control-Allow-Methods": 'OPTIONS,POST,GET', // this states the allowed methods
             "Content-Type": "application/json" // this shows the expected content type
         }
     })
